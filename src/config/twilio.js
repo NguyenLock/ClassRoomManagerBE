442c6cf82dbf5673a89962adba4c8c8527e594f5
Nguyen Lock <nlocnguyen9@gmail.com>
2025-07-11 00:09:38 +0700

    first commit

---
+++ src/config/twilio.js
@@ -0,0 +1,12 @@
+const twilio = require('twilio');
+require('dotenv').config();
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const fromPhone = process.env.TWILIO_PHONE_NUMBER;
+
+const client = twilio(accountSid, authToken);
+
+module.exports = {
+    client, fromPhone
+}
